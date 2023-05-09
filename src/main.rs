7a9d361529e4414a72cd289be1a73459b1465240
prinny enjoyer <>
2023-05-09 21:15:27 +0100

    modified:   .gitignore 	modified:   Cargo.toml 	modified:   src/main.rs 	TODO.txt 	src/login.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-//Define libraries
-use matrix_sdk::{
-        Client, config::SyncSettings,
-        ruma::{user_id, events::room::message::SyncRoomMessageEvent},
-};
-fn main() {
-    // Staring egirlm
-    println!("Starting egirlm");
-    
+/* Define libraries */
+use std::io;
+/*
+use json::{stuff}
+*/
+/* Define Libraries */
+
+fn main() { 
+    println! ("Starting egirlsm");
 }
+
