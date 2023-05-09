21de76eec8aeaa6488b3e122d6cd5ad7e7b752ee
prinny enjoyer <>
2023-05-09 21:17:30 +0100

    new file:   TODO.txt 	new file:   src/login.rs

---
+++ src/login.rs
@@ -0,0 +1,11 @@
+/* Define Libraries */
+use matrix_sdk::{
+        Client, config::SyncSettings,
+        ruma::{user_id, events::room::message::SyncRoomMessageEvent},
+};
+/* Define Libaries */
+
+
+/* Do stuff here */
+
+
