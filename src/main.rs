bc3eeac8a412a7372194322a036e961e530d4829
prinny enjoyer <>
2023-05-09 20:23:02 +0100

    new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   LICENSE 	new file:   README.md 	new file:   src/main.rs
    	.gitignore

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//Define libraries
+use matrix_sdk::{
+        Client, config::SyncSettings,
+        ruma::{user_id, events::room::message::SyncRoomMessageEvent},
+};
+fn main() {
+    // Staring egirlm
+    println!("Starting egirlm");
+    
+}
